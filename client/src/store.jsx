fa5ca6677d89947e9d5506a0ef6b657b0be03251
eXtr3me <dmfigueira@gmail.com>
2019-04-08 16:13:54 +0100

    Removed Redux DevTools Extension from Production test

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -6,11 +6,22 @@ const initialState = {};
 
 const middleware = [thunk];
 
+let reduxDevTools;
+
+if (process.env.NODE_ENV === 'production') {
+  reduxDevTools = null;
+} else {
+  reduxDevTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+}
+
 const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware)
+    applyMiddleware(...middleware),
+    reduxDevTools
     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
